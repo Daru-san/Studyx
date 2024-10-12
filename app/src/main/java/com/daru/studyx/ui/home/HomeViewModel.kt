dfd27ae5362430397f30e129cb75cbecac2f00fe
Daru <mica.tui@proton.me>
2024-10-13 01:05:42 +0200

    src: init

---
+++ app/src/main/java/com/daru/studyx/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.daru.studyx.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
