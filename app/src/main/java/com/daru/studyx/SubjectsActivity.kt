bdd7342de452e83f122751aa9adb3b12980567df
Daru <mica.tui@proton.me>
2024-10-15 18:12:55 +0200

    src/ui/subjects: init

---
+++ app/src/main/java/com/daru/studyx/SubjectsActivity.kt
@@ -0,0 +1,28 @@
+package com.daru.studyx
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.daru.studyx.databinding.ActivitySubjectsBinding
+
+class SubjectsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySubjectsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySubjectsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+}
