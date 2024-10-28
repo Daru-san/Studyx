5ed0400f035b6ed8d790936535f982d38ee43997
Daru <mica.tui@proton.me>
2024-10-28 18:51:46 +0200

    src/timetable: add datepicker with preset values

--- lib/ui/school_timetable/school_timetable.dart
+++ lib/ui/school_timetable/school_timetable.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class SchoolTimetable extends StatelessWidget {
   SchoolTimetable({super.key});
@@ -38,4 +39,112 @@ class SchoolTimetable extends StatelessWidget {
       ),
     );
   }
+}
+
+class DatePicker extends StatelessWidget {
+  DatePicker({super.key});
+
+  final DateRangePickerController _dateRangePickerController =
+      DateRangePickerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SfDateRangePicker(
+          showActionButtons: true,
+          controller: _dateRangePickerController,
+          onSubmit: (selection) {},
+          onCancel: () {
+            _dateRangePickerController.selectedRange = null;
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TimeTableDataSource extends CalendarDataSource {
+  TimeTableDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
+
+List<Appointment> getClasses() {
+  List<Appointment> classes = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 7, 45, 0);
+
+  classes.add(
+    Appointment(
+      startTime: startTime,
+      endTime: startTime.add(const Duration(minutes: 45)),
+      subject: 'Life Orientation',
+      color: Colors.grey,
+      recurrenceRule: 'FREQ=DAILY;COUNT=30;INTERVAL=3',
+    ),
+  );
+  classes.add(
+    Appointment(
+      startTime: startTime.add(const Duration(minutes: 45)),
+      endTime: startTime.add(const Duration(minutes: 90)),
+      subject: 'Physics',
+      color: Colors.green,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10;INTERVAL=2',
+    ),
+  );
+  classes.add(
+    Appointment(
+      startTime: startTime.add(const Duration(minutes: 90)),
+      endTime: startTime.add(const Duration(minutes: 135)),
+      subject: 'Life Science',
+      color: Colors.pink,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10;INTERVAL=1',
+    ),
+  );
+  classes.add(
+    Appointment(
+      startTime: startTime.add(const Duration(minutes: 135)),
+      endTime: startTime.add(const Duration(minutes: 180)),
+      subject: 'English',
+      color: Colors.brown,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10',
+    ),
+  );
+
+  classes.add(
+    Appointment(
+      startTime: startTime.add(const Duration(minutes: 180)),
+      endTime: startTime.add(const Duration(minutes: 225)),
+      subject: 'Math',
+      color: Colors.teal,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10',
+    ),
+  );
+
+  classes.add(
+    Appointment(
+      startTime: startTime.add(
+        const Duration(minutes: 225, days: 1),
+      ),
+      endTime: startTime.add(const Duration(minutes: 270, days: 1)),
+      subject: 'Afrikaans',
+      color: Colors.purpleAccent,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10;INTERVAL=2',
+    ),
+  );
+
+  classes.add(
+    Appointment(
+      startTime: startTime.add(
+        const Duration(minutes: 270, days: 2),
+      ),
+      endTime: startTime.add(const Duration(minutes: 315, days: 2)),
+      subject: 'Computer Science',
+      color: Colors.blueAccent,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10;INTERVAL=2',
+    ),
+  );
+  return classes;
 }
