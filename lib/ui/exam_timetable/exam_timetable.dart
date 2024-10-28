532a4e38b3da73650d4d1067d2cf09f9ca872381
Daru <mica.tui@proton.me>
2024-10-28 20:50:49 +0200

    src/exam_timetable: initialize timetable; convert to stateful widget

--- lib/ui/exam_timetable/exam_timetable.dart
+++ lib/ui/exam_timetable/exam_timetable.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class ExamTimetable extends StatelessWidget {
+class ExamTimetable extends StatefulWidget {
   const ExamTimetable({super.key});
+
+  @override
+  State<ExamTimetable> createState() {
+    return _ExamTimetableState();
+  }
+}
+
+class _ExamTimetableState extends State<ExamTimetable> {
+  final CalendarController _calendarController = CalendarController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Exam Timetable'),
-      ),
-      //TODO: Initialize the calendar
-      body: const Center(
-        child: Card(
-          child: ListTile(
-            leading: Icon(Icons.construction),
-            title: Text('Under construction'),
+      body: Center(
+        child: SfCalendar(
+          view: CalendarView.day,
+          firstDayOfWeek: 1,
+          controller: _calendarController,
+          timeSlotViewSettings: const TimeSlotViewSettings(
+            startHour: 7,
+            endHour: 15,
+            timeIntervalHeight: -1,
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Change view',
+        child: const Icon(Icons.view_day),
+        onPressed: () {
+          if (_calendarController.view == CalendarView.week) {
+            _calendarController.view = CalendarView.day;
+          } else {
+            _calendarController.view = CalendarView.week;
+          }
+        },
       ),
     );
   }
 }
+
