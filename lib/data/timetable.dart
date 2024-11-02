74bceb84d04efa1c9f0dc6499ffc6f40e03d5db2
Daru <mica.tui@proton.me>
2024-11-02 20:38:37 +0200

    src/data/timetable: init json formatted timetable data
    Still in early conception, but this is how I hope to structure the data
    in the json file to be read. I hope to cache it as well, to store it in
    memory until it has been edited, to minimize disk reads and writes.
    Maybe using an sql database may be better, but who knows :shrug

--- lib/data/timetable.dart
+++ lib/data/timetable.dart
@@ -1,12 +1,25 @@
+//TODO: Parse the json data within the `subjects array`
 class Timetable {
-  final DateTime startDate;
-  final DateTime endDate;
+  final DateTime startTime;
+  final DateTime endTime;
+  final String subject;
+  final String teacher;
 
-  Timetable(this.startDate, this.endDate);
+  Timetable(this.startTime, this.endTime, this.subject, this.teacher);
 
+  // This will have to be done hierarchically, with a bit more stuff
+  // like `final lessonLegth = json['lessonLegth']`
+  // Then some way to search through the array of subjects on days
   Timetable.fromJson(Map<String, dynamic> json)
-      : startDate = json['startDate'] as DateTime,
-        endDate = json['endDate'] as DateTime;
+      : startTime = json['startDate'] as DateTime,
+        endTime = json['endDate'] as DateTime,
+        subject = json['subject'] as String,
+        teacher = json['teacher'] as String;
 
-  Map<String, dynamic> toJson() => {'startDate': startDate, 'endDate': endDate};
+  Map<String, dynamic> toJson() => {
+        'startDate': startTime,
+        'endDate': endTime,
+        'subject': subject,
+        'teacher': teacher
+      };
 }
