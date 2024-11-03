63cc4387708028b700231826bd3e8d9fdba85356
Daru <mica.tui@proton.me>
2024-11-03 20:11:26 +0200

    src/data: move timetable sources

--- lib/data/timetable/timetable.dart
+++ lib/data/timetable/timetable.dart
@@ -1,9 +1,15 @@
+import 'package:studyx/data/subjects/subject.dart';
+
 //TODO: Parse the json data within the `subjects array`
 class Timetable {
   final DateTime startTime;
   final DateTime endTime;
-  final String subject;
   final String teacher;
+  // final SubjectEntriees?
+
+  //TODO: Get this as an array somehow,
+  //we need multiple items per entry in the Timetable
+  final Subject subject;
 
   Timetable(this.startTime, this.endTime, this.subject, this.teacher);
 
@@ -13,7 +19,7 @@ class Timetable {
   Timetable.fromJson(Map<String, dynamic> json)
       : startTime = json['startDate'] as DateTime,
         endTime = json['endDate'] as DateTime,
-        subject = json['subject'] as String,
+        // subject = json['subject'] as String,
         teacher = json['teacher'] as String;
 
   Map<String, dynamic> toJson() => {
@@ -23,3 +29,20 @@ class Timetable {
         'teacher': teacher
       };
 }
+
+//NOTE: This is very likely to change
+class SubjectEntry {
+  final DateTime startTime;
+  final DateTime endTime;
+  final DateTime day;
+  final Subject subject;
+  SubjectEntry(this.day, this.startTime, this.endTime, this.subject);
+}
+
+/*
+*NOTE:
+* This is all very subject to change, I hope to learn
+* sql and then use databases to store all of this data
+* rather than using json files, it seems like that would
+* be much cleaner than using a json file
+* */
