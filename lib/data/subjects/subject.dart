0af009825c9970a4b6a066989f6b21b341608ed3
Daru <mica.tui@proton.me>
2024-11-13 22:30:13 +0200

    data/subject: add more properties and remove future insertion
    Still in progress but this is a small step towards getting data saving

--- lib/data/subjects/subject.dart
+++ lib/data/subjects/subject.dart
@@ -1,35 +1,41 @@
-import 'package:sqflite/sqlite_api.dart';
-import 'package:studyx/main.dart';
+import 'package:sqflite/sqflite.dart';
 
 class Subject {
   final int id;
   final String name;
   final String teacher;
+  final DateTime startTime;
+  final DateTime endTime;
+  final DateTime room;
 
-  //NOTE: May scrap this for something better
-  final String classNum;
-  Subject(this.name, this.teacher, this.classNum);
+  Subject(
+      {required this.name,
+      required this.id,
+      required this.teacher,
+      required this.room,
+      required this.startTime,
+      required this.endTime,});
 
-  Map<String, Object?> toMap() {
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
       'teacher': teacher,
+      'room': room,
+      'startTime':startTime,
+      'endTime':endTime,
     };
   }
 
+  static Subject fromMap(<String,dynamic> map) {
+    return Subject (
+      id: map['id'],
+      name: Map()
+    );
+  }
+
   @override
-    String toString() {
-      return('Subject{id: $id, name: $name, teacher: $teacher}');
-    }
-}
-
-Future<void> insertSubject(Subject subject){
-  final db = await appDatabase;
-
-  await db.insert(
-  'subjects',
-  subject.toMap(),
-  ConflictAlgorithm:ConflictAlgorithm.replace,
-  );
+  String toString() {
+    return ('Subject{id: $id, name: $name, teacher: $teacher}');
+  }
 }
