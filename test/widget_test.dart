4eb413d55c201d5ddac25fddc11e96a347942247
Daru <mica.tui@proton.me>
2024-11-14 18:43:53 +0200

    test: add theme parameter to app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:studyx/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const StudyxApp());
+    await tester.pumpWidget(StudyxApp(
+      theme: ThemeData.dark(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -28,3 +30,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
