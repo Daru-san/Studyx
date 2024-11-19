8639a26ed0207b6bf98e63248e95f2e4207acd8e
Daru <mica.tui@proton.me>
2024-11-19 15:15:29 +0200

    tests: remove counter tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,23 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:studyx/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test app', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(StudyxApp(
       theme: ThemeData.dark(),
     ));
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
-
